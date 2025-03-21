863d7dbe01a81fc4a7d9a9ec11998e18313f045f
pongdang04 <pongdangdang419@gmail.com>
2025-03-21 19:45:52 +0900

    NewUserChart in Statistics Page

--- frontend/src/Pages/statistics.tsx
+++ frontend/src/Pages/statistics.tsx
@@ -1,6 +1,6 @@
 import NavigationBar from "../Components/NavigationBar";
 import styled from "styled-components";
-
+import NewUserChart from "../Components/NewUserChart";
 const ContentWrapper = styled.div`
   padding: 20px; /* 내용에 여백 추가 */
   z-index: 1;
@@ -17,7 +17,7 @@ const StatisticsPage = () => {
       <PageContainer>
         <NavigationBar />
         <ContentWrapper>
-          <div>This is statistics Page</div>
+          <NewUserChart />
         </ContentWrapper>
       </PageContainer>
     </>
